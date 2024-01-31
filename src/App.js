cc634f5ae7d920aa30ce7eb2a85a6e1d11525565
drin <132683845+Drinisit@users.noreply.github.com>
2024-01-31 11:03:07 +0800

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
-import Nav from './components/Nav.jsx'
+import {BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
-      <Nav />
-    </>
+    
+      
+      <BrowserRouter>
+       <div>
+          <header>
+            <nav>
+              <h1>L-NU Library</h1>
+              <NavLink to="/" >Home</NavLink>
+              <NavLink to="/services">Services</NavLink>
+              <NavLink to="/about">About</NavLink>
+              
+            </nav>
+          </header>
+
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    
+ 
   );
 }
 
