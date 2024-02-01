5c479c250c3dcc2040d73377533eda60557e1077
drin <132683845+Drinisit@users.noreply.github.com>
2024-02-01 14:37:21 +0800

    Add: hero section, Fix: searchbar responsive

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,11 +5,11 @@ const Search = () => {
   return (
     <>
       <div className='w-full bg-gray-200'>
-        <div className='max-w-[1240px] mx-auto py-6'>
+        <div className='max-w-[1240px] mx-auto md:py-6 py-2'>
             <div className='flex justify-center'>
-            <input className='border border-red-900 rounded-l-md w-10/12 py-1 px-4 mr-1' type="text" placeholder='Search books'/>
+            <input className='border border-red-900 rounded-l-md md:w-10/12 w-1/2 md:h-9 h-6 text-sm py-1 px-4 mr-1' type="text" placeholder='Search books'/>
 
-            <CiSearch className='text-4xl text-white bg-red-900 rounded-r-md p-1'/>
+            <CiSearch className='md:text-4xl text-2xl text-white bg-red-900 rounded-r-md p-1'/>
             </div>
          
         </div>
