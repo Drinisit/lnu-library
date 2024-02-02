7b5724e0d2a53105ff08156b61a9e0e273ae621d
drin <132683845+Drinisit@users.noreply.github.com>
2024-02-02 11:06:23 +0800

    Add: gap display book section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,11 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Search from '../components/Search'
 import Display from '../components/Display'
-import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Hero />
       <Search/>
       <Display />
     </div>
