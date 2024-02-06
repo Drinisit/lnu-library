853846cb46bf01e2ebfd7cfc6e69a19916846c36
Drin <aldrinisit3196@gmail.com>
2024-02-07 06:27:41 +0800

    Add: info display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
-import Img1 from '../assets/img4.png'
+
 
 const Display = () => {
   return (
     <div className='w-full'>
         <div className='max-w-[1240px] mx-auto px-5 md:py-8 py-4'>
 
-            <div className='grid md:grid-cols-3 grid-cols-1 bg-slate-400'>
-               <div className='md:col-span-3 bg-slate-300'>
-                  <h1 className='font-semibold uppercase md:text-2xl text-xl md:text-start text-center'>Book and e-books</h1>
-              </div>
-              
-                  <div className='bg-amber-500'>
-                    <img className='h-56' src={Img1} alt="" />
-                  </div>
-                  <div className='bg-amber-300 md:col-span-2 col-span-1'>
-                    <img className='h-56' src={Img1} alt="" />
-                  </div>
+            <div className="flex gap-2">
+                <div className="bg-gray-300">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, laborum recusandae accusantium minima at eaque, neque officia consequuntur sunt nisi molestias, dignissimos tempore illo odio voluptate temporibus laudantium doloremque itaque!
+                  Omnis blanditiis excepturi inventore corrupti eaque consectetur in quae quo odio atque expedita, explicabo, molestias voluptatem quod quas. Iure quis iusto nihil cum quae minima deserunt natus? Enim, nobis voluptatibus!
+                  Temporibus, id quidem ducimus molestias perspiciatis non dolorem quaerat. Rem autem ipsam quo dicta! Doloremque mollitia, neque illo quod eveniet reprehenderit voluptas, fugiat non ullam autem voluptatibus incidunt, quo provident!
+                  Eum doloribus vel similique sit nostrum dicta provident aliquid, magni, porro qui harum, nemo nulla error omnis deserunt excepturi libero quisquam corrupti? Aperiam iste perferendis assumenda. Nihil, voluptatum! Hic, laboriosam!
+                  Quisquam consequatur, ut dolor animi distinctio, incidunt mollitia laboriosam nemo similique perferendis iste corrupti repudiandae quia doloribus blanditiis, consequuntur quis? Fugit ipsam error, perspiciatis sed soluta nobis! Corporis, iste! Autem.</p>
+                </div>
 
+                <div className="bg-amber-300">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, obcaecati labore! Laborum pariatur unde quidem suscipit facere quaerat doloribus, delectus tempore eos consectetur deleniti eius perferendis repellendus facilis. Repellendus, cumque.
+                  Id iusto ea nam eligendi tempora deserunt accusamus voluptatibus ratione esse incidunt fugit doloribus quae vero deleniti, quis aperiam, assumenda magni tenetur qui sunt sequi ex dolorum quidem? Facere, ducimus?
+                  Eius dolore repellat illum ducimus doloribus natus, non eveniet blanditiis. Veritatis omnis odit magnam eius aliquam? Doloribus quibusdam, quasi eum quia magni recusandae veniam, cum totam, impedit molestiae atque accusantium!
+                  Asperiores officia voluptas at rem quibusdam numquam aut quia ipsum recusandae maxime, similique dolorum in tempore, vitae, animi obcaecati alias est quas cupiditate ratione ad. Dolorum inventore quia culpa magni?</p>
+                </div>
 
+                <div className="bg-orange-300">
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, maiores repudiandae earum iure tenetur quia dicta assumenda provident animi accusamus quod id suscipit nulla maxime eligendi officiis. Saepe, unde possimus.
+                  Totam non id, aspernatur eum animi aperiam ducimus accusamus sunt, voluptatum consectetur beatae corrupti error vitae officia iste aut quasi quaerat nulla voluptatibus magnam quae dolor officiis quos eaque. Debitis?
+                  Iste, voluptate excepturi? Veritatis architecto eius adipisci animi voluptates natus molestiae repudiandae corrupti doloribus culpa illo mollitia atque cupiditate veniam nemo praesentium, hic accusantium porro sed? Sint dolore ad odio.</p>
+                </div>
 
-
+                <div>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste consequuntur adipisci, ducimus aliquam, similique, blanditiis iure molestias rem non eos quaerat sed perspiciatis eius totam. Pariatur, nisi illo! Laboriosam, saepe.
+                  Magni architecto temporibus deserunt, exercitationem alias rem, praesentium impedit perferendis labore nam omnis. Dolores natus quam eum odio porro possimus, laudantium voluptatum nam recusandae est maxime inventore aperiam fuga repellendus!
+                  Quod accusantium sed porro, excepturi enim voluptas quae debitis sint et, quidem nobis, reprehenderit facere vero? Quisquam ipsum voluptatem exercitationem. Quas nulla velit iste quis, ducimus harum fuga repellendus maxime!
+                  Vero sint architecto culpa voluptatem iusto, praesentium magni harum reprehenderit temporibus fugit alias tempora doloremque error nulla a voluptas? Accusamus, dolor incidunt. Consequatur repudiandae autem aut reiciendis ab sunt iusto.
+                  Voluptatum vitae excepturi soluta, harum dolore quos ea placeat maiores dolorem dolorum quas deserunt modi tenetur accusantium veritatis animi unde doloribus fugiat. Rerum aperiam aspernatur rem similique! Tempore, dolor consequuntur.
+                  A sit sed possimus totam non quaerat autem laborum dolor, praesentium tenetur id mollitia illum iure officiis sapiente deserunt error accusantium nesciunt consequuntur. Obcaecati vero facilis architecto illum voluptate. Ut.</p>
+                </div>
             </div>
+           
+
+
+
+
+           
             
         </div>
     </div>
